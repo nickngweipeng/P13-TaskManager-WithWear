940f3c36908ecf20c43e1b728ab8a45f4eae0183
nickngweipeng <15056201@myrp.edu.sg>
2017-08-17 20:03:29 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p06_taskmanager/Task.java
@@ -0,0 +1,40 @@
+package com.myapplicationdev.android.p06_taskmanager;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+
+	private int id;
+	private String name, description;
+
+	public Task(int id, String name, String description) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return id + " " + name + "\n" + description;
+	}
+}
